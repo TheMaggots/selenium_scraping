342840f0143276bd8c47cb32ade0841d4df87400
memetics19 <shreeda.bhat@yahoo.com>
2020-06-30 21:18:55 +0530

    Updated Javascript Executor

--- Browser.py
+++ Browser.py
@@ -11,16 +11,20 @@ BASE_URL = "https://google.com"
 """ Base_URL is been passed to driver"""
 driver.get(BASE_URL) 
 
-search = "Coronavirus vaccine related research filetype:pdf"
+search = "spanish flu filetype:pdf"
 search_box = driver.find_element_by_xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input").send_keys(search,Keys.RETURN)
 
 """ All the href will be stored in results """
 
+def GetLinks():
+    for a in driver.find_elements_by_xpath("//a"):
+        """ Finding the element by using xpath """ 
+        href = a.get_attribute("href") ### Getting all the href and storing the list 
+        print(href)
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        driver.find_element_by_xpath('/html/body/div[5]/div[2]/div[9]/div[1]/div[2]/div/div[5]/div[2]/span[1]/div')
 
-for a in driver.find_elements_by_xpath("//a"):
-    """ Finding the element by using xpath """ 
-    href = a.get_attribute("href") ### Getting all the href and storing the list 
-    print(href)
+GetLinks()
 
 
 driver.close()
